bb93d99d71832a7829aaabde56cd86cea362aa41
Cameron Cooke <cameroncooke@notonthehighstreet.com>
2022-07-08 17:01:55 +0100

    Initial commit

---
+++ swift/Tests/GildedRoseTests/GildedRoseTests.swift
@@ -0,0 +1,15 @@
+import Foundation
+import XCTest
+
+@testable import GildedRose
+
+class GildedRoseTests: XCTestCase {
+
+    func testFoo() {
+        let items = [Item(name: "foo", sellIn: 0, quality: 0)]
+        let app = GildedRose(items: items);
+        app.updateQuality();
+        
+        XCTAssertEqual("fixme", app.items[0].name);
+    }
+}
