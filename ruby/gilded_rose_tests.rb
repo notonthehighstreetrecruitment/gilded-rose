5ecfb3ad9b3ac53e4123a8b3a076c776dccaf2c2
Simon Chenbo Yi <yichenbo@outlook.com>
2024-02-27 09:25:06 +0000

    fix current test and rubocop

--- ruby/gilded_rose_tests.rb
+++ ruby/gilded_rose_tests.rb
@@ -1,12 +1,14 @@
+#!/usr/bin/ruby -w
+
+# frozen_string_literal: true
+
 require File.join(File.dirname(__FILE__), 'gilded_rose')
 require 'test/unit'
 
 class TestUntitled < Test::Unit::TestCase
-
   def test_foo
-    items = [Item.new("foo", 0, 0)]
-    GildedRose.new(items).update_quality()
-    assert_equal items[0].name, "fixme"
+    items = [Item.new('foo', 0, 0)]
+    GildedRose.new(items).update_quality
+    assert_equal items[0].name, 'foo'
   end
-
 end
